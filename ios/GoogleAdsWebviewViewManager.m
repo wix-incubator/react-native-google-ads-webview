2017a389851cc27b17bffe4abbbda4ff123da849
Shahar David <shahard333@gmail.com>
2022-12-26 14:58:12 +0200

    * Convert iOS module from objective C to Swift

--- ios/GoogleAdsWebviewViewManager.m
+++ ios/GoogleAdsWebviewViewManager.m
@@ -1,34 +1,7 @@
 #import <React/RCTViewManager.h>
 
-@interface GoogleAdsWebviewViewManager : RCTViewManager
-@end
+@interface RCT_EXTERN_MODULE(GoogleAdsWebviewViewManager, RCTViewManager)
 
-@implementation GoogleAdsWebviewViewManager
-
-RCT_EXPORT_MODULE(GoogleAdsWebviewView)
-
-- (UIView *)view
-{
-  return [[UIView alloc] init];
-}
-
-RCT_CUSTOM_VIEW_PROPERTY(color, NSString, UIView)
-{
-  [view setBackgroundColor:[self hexStringToColor:json]];
-}
-
-- hexStringToColor:(NSString *)stringToConvert
-{
-  NSString *noHashString = [stringToConvert stringByReplacingOccurrencesOfString:@"#" withString:@""];
-  NSScanner *stringScanner = [NSScanner scannerWithString:noHashString];
-
-  unsigned hex;
-  if (![stringScanner scanHexInt:&hex]) return nil;
-  int r = (hex >> 16) & 0xFF;
-  int g = (hex >> 8) & 0xFF;
-  int b = (hex) & 0xFF;
-
-  return [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:1.0f];
-}
+RCT_EXPORT_VIEW_PROPERTY(color, NSString)
 
 @end
--- react-native-google-ads-webview.podspec
+++ react-native-google-ads-webview.podspec
@@ -14,7 +14,7 @@ Pod::Spec.new do |s|
   s.platforms    = { :ios => "11.0" }
   s.source       = { :git => "https://github.com/shahardavid/react-native-google-ads-webview.git", :tag => "#{s.version}" }
 
-  s.source_files = "ios/**/*.{h,m,mm}"
+  s.source_files = "ios/**/*.{h,m,mm,swift}"
 
   s.dependency "React-Core"
 
