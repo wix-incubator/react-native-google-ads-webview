24a3ad535c88e5248df6027aa6bbebe5c3fa75d2
Shahar David <shahard333@gmail.com>
2023-01-17 14:40:37 +0200

    * Update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
 import { GoogleAdsWebviewView } from 'react-native-google-ads-webview';
 
+const TEST_AD_PROPS = {
+  adHost: 'ca-host-pub-should-generated-by-google',
+  googleTestAdClient: 'ca-pub-9952238966586479',
+  googleTestAdPageUrl: 'https://amirhr.com/',
+};
+
 export default function App() {
+  const [isUnfilledAd, setIsUnfilledAd] = React.useState(false);
+
+  const handleUnfilledAd = () => {
+    setIsUnfilledAd(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <GoogleAdsWebviewView color="#32a852" style={styles.box} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Top Content</Text>
+      {!isUnfilledAd && (
+        <GoogleAdsWebviewView
+          style={{ width: 300, height: 250 }}
+          adHost={TEST_AD_PROPS.adHost}
+          adClient={TEST_AD_PROPS.googleTestAdClient}
+          pageUrl={TEST_AD_PROPS.googleTestAdPageUrl}
+          onUnfilledAd={handleUnfilledAd}
+        />
+      )}
+      <Text>Bottom Content</Text>
+    </SafeAreaView>
   );
 }
 
@@ -16,10 +38,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
   },
 });
